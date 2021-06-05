bd70cbec73ecd46b919a667ae97e71a6ade27db6
FMagnani <federico.magnani5@studio.unibo.it>
2021-06-05 23:14:25 +0200

    Tried to adapt the code from https://github.com/pierremtb/PINNs-TF2.0/blob/master/utils/neuralnetwork.py but with no results. I will resume and try again later.

--- Schrodinger/NeuralNets.py
+++ Schrodinger/NeuralNets.py
@@ -9,7 +9,10 @@ Created on Fri Jun  4 18:41:40 2021
 
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-
+import time
+from tqdm import tqdm
+import numpy as np
+from LBFGS import Struct, lbfgs
 
 #%%
 
@@ -17,6 +20,9 @@ class neural_net(tf.keras.Model):
     
     def __init__(self, ub, lb, hidden_dim=100):
         super(neural_net, self).__init__()
+        
+        self.layers_list = [2, 100, 100, 100, 100, 2]
+        self.output_type = 'float64'
         
         self.lb = lb
         self.ub = ub
@@ -38,7 +44,7 @@ class neural_net(tf.keras.Model):
 
         self.last_layer = Dense(self.out_dim,
                     bias_initializer="zeros",
-                    dtype = 'float64')
+                    dtype = self.output_type)
 
     def call(self, X):
         
@@ -76,8 +82,18 @@ class Schrodinger_PINN():
         self.t_f = t_f
         self.X_star = X_star
         
+    
+    def train(self, n_iterations, optimizer=tf.keras.optimizers.Adam()):
+    
+        start_time = time.time()    
+        #Train step
+        for _ in tqdm(range(n_iterations)):
+            self.train_step(optimizer)
+        elapsed = time.time() - start_time                
+        print('Training time: %.4f' % (elapsed))
+    
         
-    def train_step(self, n_iterations, optimizer):
+    def train_step(self, optimizer):
         
         with tf.GradientTape() as tape:
             loss_value = self.loss(self.x0,self.t0, self.u0,self.v0)
@@ -179,6 +195,115 @@ class Schrodinger_PINN():
         f_v = v_t - 0.5*u_xx - (u**2 + v**2)*u   
     
         return u, v, f_u, f_v
+
+
+
+
+class Schrod_PINN_LBFGS(Schrodinger_PINN):
+    
+    def __init__(self, x0, u0, v0, x_ub, x_lb, t_b, x_f, t_f, X_star, ub, lb, hidden_dim=100):
+        super(Schrod_PINN_LBFGS, self).__init__(x0, u0, v0, x_ub, x_lb, t_b, x_f, t_f, X_star, ub, lb, hidden_dim)
+    
+        # Setting up the quasi-newton LBGFS optimizer 
+        # (set nt_epochs=0 to cancel it)
+        self.nt_epochs = 2000
+        self.nt_config = Struct()
+        self.nt_config.learningRate = 0.8
+        self.nt_config.maxIter = self.nt_epochs
+        self.nt_config.nCorrection = 50
+        self.nt_config.tolFun = 1.0 * np.finfo(float).eps
+        
+        # Computing the sizes of weights/biases for future decomposition
+        self.sizes_w = []
+        self.sizes_b = []
+        # layers is something like [2, 100, 100, 100, 100, 2]
+#        for i, width in enumerate(layers):
+        for i, width in enumerate([2, 100, 100, 100, 100, 2]):    
+            if i != 1:
+#                self.sizes_w.append(int(width * layers[1]))
+#                self.sizes_b.append(int(width if i != 0 else layers[1]))
+                self.sizes_w.append(int(width * 100))
+                self.sizes_b.append(int(width if i != 0 else 100))
+    
+    def train(self, n_iterations, optimizer=tf.keras.optimizers.Adam()):
+        
+        start_time = time.time()    
+        #Train step
+        for _ in tqdm(range(n_iterations)):
+            self.train_step(optimizer)
+        elapsed = time.time() - start_time                
+        print('Training time: %.4f' % (elapsed))
+    
+        def loss_and_flat_grad(w):
+    
+            with tf.GradientTape() as tape:
+                self.set_weights(w)
+            
+                loss_value = self.loss(self.x0,self.t0, self.u0,self.v0)
+            
+            grad = tape.gradient(loss_value, self.model.trainable_variables)
+            grad_flat = []
+            for g in grad:
+                grad_flat.append(tf.reshape(g, [-1]))
+
+            # MY ADDITION
+            grad_flat = tf.cast(grad_flat, dtype='float32')
+
+            grad_flat =  tf.concat(grad_flat, 0)
+            
+            return loss_value, grad_flat
+    
+        lbfgs(loss_and_flat_grad,
+              self.get_weights(),
+              self.nt_config, Struct())
+
+    
+    
+    def get_weights(self):    
+        w = []
+        
+#        for layer in self.model.layers[1:]:
+        for layer in self.model.layers:   
+            weights_biases = layer.get_weights()
+            weights = weights_biases[0].flatten()
+            biases = weights_biases[1]
+            w.extend(weights)
+            w.extend(biases)
+        return tf.convert_to_tensor(w, dtype='float32')
+
+    def set_weights(self, w):
+#        for i, layer in enumerate(self.model.layers[1:]):
+        for i, layer in enumerate(self.model.layers):
+            start_weights = sum(self.sizes_w[:i]) + sum(self.sizes_b[:i])
+            end_weights = sum(self.sizes_w[:i+1]) + sum(self.sizes_b[:i])
+            weights = w[start_weights:end_weights]
+            w_div = int(self.sizes_w[i] / self.sizes_b[i])
+            weights = tf.reshape(weights, [w_div, self.sizes_b[i]])
+            biases = w[end_weights:end_weights + self.sizes_b[i]]
+            weights_biases = [weights, biases]
+            layer.set_weights(weights_biases)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
