ab1efd9feb42307f990f105b3f413d6dc30c9b07
FMagnani <federico.magnani5@studio.unibo.it>
2021-06-05 18:03:23 +0200

    Updated loss, implemented net_uv

--- Schrodinger/Utilities.py
+++ Schrodinger/Utilities.py
@@ -17,8 +17,6 @@ import tensorflow as tf
     # - Compute the gradient of the loss wrt the model weights (trainable vars)
     # - Apply the gradients (optimizer does)
     
-# Access to model guaranteed
-# Data needed: input and their labels for prediction, iterations, optimizer
 def train_step(x0,t0, u0,v0, n_iterations, optimizer):
         
     with tf.GradientTape() as tape:
@@ -42,24 +40,37 @@ def loss(x0,t0, u0,v0):
     f_u, f_v = net_f_uv()
     yS = tf.reduce_mean(tf.square(f_u)) + \
           tf.reduce_mean(tf.square(f_v))
-
-    return y0 + yS
-
-# def net_uv(x, t):
+          
+    # Loss from boundary conditions
+    u_lb_pred, v_lb_pred, u_x_lb_pred, v_x_lb_pred = net_uv(x_lb, t_lb)
+    u_ub_pred, v_ub_pred, u_x_ub_pred, v_x_ub_pred = net_uv(x_ub, t_ub)
     
-#     X = tf.concat([x,t],1)
-#     X = tf.Variable(X)
+    yB = tf.reduce_mean(tf.square(u_lb_pred - u_ub_pred)) + \
+         tf.reduce_mean(tf.square(v_lb_pred - v_ub_pred)) + \
+         tf.reduce_mean(tf.square(u_x_lb_pred - u_x_ub_pred)) + \
+         tf.reduce_mean(tf.square(v_x_lb_pred - v_x_ub_pred))
     
-#     with tf.GradientTape(persistent=True) as tape:
-#         u, v = model(X)
+
+    return y0 + yS + yB
+
+
+
+def net_uv(x, t):
+    
+    with tf.GradientTape(persistent=True) as tape:
+        
+        tape.watch(x)
+        tape.watch(t)
+        X = tf.stack([x, t], axis=1) # shape = (N_f,2)
+        
+        u, v = model(X)
          
-#     u_X = tape.gradient(u, X)
-#     v_X = tape.gradient(v, X)
+    u_x = tape.gradient(u, x)
+    v_x = tape.gradient(v, x)
 
-#     u_x = u_X[:,0:1]
-#     v_x = v_X[:,1:2]
+    return u, v, u_x, v_x
 
-#     return u, v, u_x, v_x
+
 
 def create_net_f_uv(x_f, t_f):
     
