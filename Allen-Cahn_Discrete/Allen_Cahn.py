fbdfe26f029ff62b98ce0f9d81a2f8e91936a49d
FMagnani <federico.magnani5@studio.unibo.it>
2021-06-20 19:01:07 +0200

    Completed

--- Allen-Cahn_Discrete/Allen_Cahn.py
+++ Allen-Cahn_Discrete/Allen_Cahn.py
@@ -12,11 +12,14 @@ import scipy.io
 import tensorflow as tf
 
 from Allen_Cahn_PINN import Allen_Cahn_PINN
+from plotting import plot_results
 
 #%%
 
 if __name__ == "__main__": 
         
+    ###    MAIN    ###
+    
     q = 100
     layers = [1, 200, 200, 200, 200, q+1]
     lb = np.array([-1.0])
@@ -51,21 +54,31 @@ if __name__ == "__main__":
 
     model = Allen_Cahn_PINN(x0, u0, layers, dt, lb, ub, q)
     
+#%%
+
+    ###    TRAINING    ###
+
+    adam_iterations = 100      # Number of training steps 
+    lbfgs_max_iterations = 100 # Max iterations for lbfgs
+    
+    model.train(adam_iterations, lbfgs_max_iterations)
+        
 
 
+#%%
 
+    ###    PREDICTION    ###
 
+    U1_pred = model.predict(x_star)
 
+    error = np.linalg.norm(U1_pred[:,-1] - Exact[idx_t1,:], 2)/np.linalg.norm(Exact[idx_t1,:], 2)
+    print('Error: %e' % (error))
 
+#%%
 
+    ###    PLOTTING    ###
 
-
-
-
-
-
-
-
+    plot_results(U1_pred, Exact,t,x_star,x,idx_t0,idx_t1,x0,u0,lb,ub)
 
 
 
