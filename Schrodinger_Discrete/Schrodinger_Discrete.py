1ba2dcff83081c64e8384d66f3864ffb15b233a1
FMagnani <federico.magnani5@studio.unibo.it>
2021-06-22 14:04:33 +0200

    Started to implement Discrete Time Model for Schrodinger example

---
+++ Schrodinger_Discrete/Schrodinger_Discrete.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 11:57:42 2021
+
+@author: FMagnani
+
+"""
+
+import numpy as np
+import tensorflow as tf
+import scipy.io
+import matplotlib.pyplot as plt
+
+from Schrodinger_Discrete_PINN import Schrodinger_PINN
+from plotting import plot_slice
+
+
+#%%
+
+if __name__ == "__main__":  
+    
+    # Set random seed
+    np.random.seed(1234)
+    tf.random.set_seed(1234)
+
+    # Network architecture    
+    q = 100
+    layers = [1, 200, 200, 200, 200, 2*(q+1)]
+    
+    # Domain bounds
+    lb = np.array([-5.0, 0.0])          # left bottom corner
+    ub = np.array([5.0, np.pi/2])       # right upper corner
+
+    N = 200     # Number of training pts from x=0 <- The only needed!
+
+    ###    DATA PREPARATION    ###
+     
+    # Import data
+    data = scipy.io.loadmat('../Data/NLS.mat')
+
+    
+    t = data['tt'].flatten()[:,None]
+    x = data['x'].flatten()[:,None]
+    Exact = data['uu']
+    Exact_u = np.real(Exact).T
+    Exact_v = np.imag(Exact).T
+
+    Exact_h = np.sqrt(Exact_u**2 + Exact_v**2)
+
+    # t slices: 75, 100, 125
+
+    idx_t0 = 0
+    idx_t1 = 75
+    dt = t[idx_t1] - t[idx_t0]
+
+    idx_x = np.random.choice(Exact_u.shape[1], N, replace=False) 
+
+    # Initial data - u
+    noise_u0 = 0.0
+    x0 = x[idx_x,:]
+    u0 = Exact_u[idx_t0:idx_t0+1,idx_x].T
+    u0 = u0 + noise_u0*np.std(u0)*np.random.randn(u0.shape[0], u0.shape[1])
+
+    # Initial data - v
+    noise_v0 = 0.0
+    x0 = x[idx_x,:]
+    v0 = Exact_v[idx_t0:idx_t0+1,idx_x].T
+    v0 = v0 + noise_v0*np.std(u0)*np.random.randn(u0.shape[0], u0.shape[1])
+
+    # Test data
+    x_star = x
+    
+    # Conversion to tensors    
+    x0 = tf.convert_to_tensor(x0)
+    u0 = tf.convert_to_tensor(u0)
+    v0 = tf.convert_to_tensor(v0)
+    x_star = tf.convert_to_tensor(x_star)
+
+    model = Schrodinger_PINN(x0, u0,v0, layers, dt, lb, ub, q)
+
+
+
+#%%
+
+    ###    TRAINING    ###
+
+    adam_iterations = 40     # Number of training steps 
+    lbfgs_max_iterations = 40 # Max iterations for lbfgs
+    
+    model.train(adam_iterations, lbfgs_max_iterations)
+
+
+#%%
+
+    U1_pred, V1_pred = model.predict(x_star)
+    h_pred = U1_pred[:, -1:]**2 + V1_pred[:, -1:]**2
+
+#%%
+
+    ###    PLOTTING    ###
+    
+    plot_slice(Exact_h,h_pred, 75, x_star,t)
+    plt.show()
+
+
+
+
+
+
